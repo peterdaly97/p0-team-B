d8a0f633ff1244e47f9865dd985b89e7e39dc4c3
Pierce-James <C00203711@itcarlow.ie>
2018-09-25 13:03:13 +0100

    Added mod_first implementation

--- src/p0TeamB.js
+++ src/p0TeamB.js
@@ -1,6 +1,15 @@
 /*! p0TeamB v0.0.0 - MIT license */
 'use strict';
 
-var p0TeamB = function (x) {
-  return Cube(x)
+var p0TeamB = function (list) {
+  //List of numbers passed into function
+  var n = list[0];
+
+  //Cube all values in list
+  for (var i = 0; i < list.length; i++)
+  {
+    //Cube
+    list[i] = list[i] * list[i] * list[i];
+    list = module_first(list);
+  }
 }
